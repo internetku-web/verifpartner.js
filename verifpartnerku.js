c193a8713cc811cc0e2b65cbc0eb23c9c67262aa
internetku-web <63555554+internetku-web@users.noreply.github.com>
2020-04-13 01:26:34 +0700

    Create verifpartnerku.js

---
+++ verifpartnerku.js
@@ -0,0 +1,24 @@
+#internetku{
+width:100%;
+height:auto;
+text-align:center;
+margin: 5px auto;
+}
+#internetku img{
+width:50%;
+height:100px;
+}
+#internetku h3{
+font-weight:bold;
+font-size:16px;
+font-family:'roboto', sans-serif;
+text-transform:uppercase;
+}
+#internetku p{
+color:#ddd;
+text-align:left;
+;
+#internetku p a{
+color:#1e90ff;
+font-weight:bold;
+}
